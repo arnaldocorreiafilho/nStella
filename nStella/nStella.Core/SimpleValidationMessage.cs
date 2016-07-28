b707272a24b6683ae68844726aec9ce7a5206e5e
heitor.barros <heitor@dilady.com.br>
2016-07-28 15:50:07 -0300

    Adicionado o simpleValidationMessage

---
+++ nStella/nStella.Core/SimpleValidationMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core
+{
+    public class SimpleValidationMessage : IValidationMessage
+    {
+        private readonly string message;
+        public SimpleValidationMessage(string message)
+        {
+            this.message = message;
+        }
+
+        public string GetMessage()
+        {
+            return this.message;
+        }
+        public override string ToString()
+        {
+            return GetMessage();
+        }
+    }
+}
