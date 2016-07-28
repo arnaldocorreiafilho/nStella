50ba5ed784a109765603ea0d5d0555bcddfe055d
heitor.barros <heitor@dilady.com.br>
2016-07-28 17:44:28 -0300

    Adicionado o IValidator

---
+++ nStella/nStella.Core/Validation/IValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Validation
+{
+    public interface IValidator<T>
+    {
+        void AssertValid(T Object);
+        List<IValidationMessage> InvalidMessageFor(T Object);
+        bool IsEligible(T Object);
+        T GenerateRandomValid();
+    }
+}
