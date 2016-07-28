046974bf7065449bb75acebb650aa1ead15da6f7
heitor.barros <heitor@dilady.com.br>
2016-07-28 15:34:24 -0300

    Enviando

---
+++ nStella/nStella.Core/DigitoGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core
+{
+    public class DigitoGenerator
+    {
+        private static readonly Random RANDOM = new Random();
+        public string generate(int quantidade)
+        {
+            StringBuilder digitos = new StringBuilder();
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                digitos.Append(RANDOM.Next());
+
+            }
+            return digitos.ToString();
+        }
+    }
+}
