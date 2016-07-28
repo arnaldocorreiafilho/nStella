f49d592da09356d36f448854c0d118754c9c6d2b
heitor.barros <heitor@dilady.com.br>
2016-07-28 16:25:21 -0300

    Adicionado o SimpleMessageProducer

---
+++ nStella/nStella.Core/SimpleMessageProducer.cs
@@ -0,0 +1,39 @@
+﻿using nStella.Core.Validation;
+using System.Text.RegularExpressions;
+
+namespace nStella.Core
+{
+    /// <summary>
+    /// SimpleMessageProducer é responsável pela geração de mensagens de erro. Estas                           
+    ///mensagens são geradas atraves dos nomes das anotoções que representam os
+    ///erros.
+    ///</p>
+    ///<p>                                                                                                    
+    ///A messagem de erro é composta do seguinte modo:                                                        
+    ///</p>                                                                                                   
+    ///<code>                                                                                                 
+    ///String message = (simpleName + "." + errorName).replaceFirst("[.]", " : ").replaceAll("_", " ");       
+    ///</code>                                                                                                                                                                                                       
+    ///<p>                                                                                                    
+    ///Veja o exemplo:                                                                                        
+    ///</p>                                                                                                   
+    ///<p>                                                                                                    
+    ///A mesagem do erro representado por CPFError.INVALID_DIGITS é : <br>                                      
+    ///CPFError : INVALID DIGITS .                                                                            
+    ///</p>                                                                                                   
+    /// </summary>
+    public class SimpleMessageProducer : IMessageProducer
+    {
+        public IValidationMessage GetMessage(IInvalidValue error)
+        {
+            string simpleName = error.GetType().Name;
+            string errorName = error.Name();
+            string key = simpleName + "." + errorName;
+            string message;
+            Regex regx = new Regex("");
+            message = regx.Replace(key, "[.]", 1).Replace("_", " ").ToString();
+
+            return new SimpleValidationMessage(message);
+        }
+    }
+}
