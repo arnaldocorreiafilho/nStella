7d726310d771efb50f233ea703322373a1f82c6f
heitor.barros <heitor@dilady.com.br>
2016-07-28 15:45:45 -0300

    Adicionando o MessageProducer e o InvalidValue

---
+++ nStella/nStella.Core/IMessageProducer.cs
@@ -0,0 +1,16 @@
+﻿using nStella.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core
+{
+    public interface IMessageProducer
+    {
+        /// <param name="invalidValue">Valor inválido ao qual se procura a mensagem associada.</param>
+        /// <returns>Retorna uma mensagem de validação associada ao erro.</returns>
+        IValidationMessage GetMessage(IInvalidValue invalidValue);
+    }
+}
