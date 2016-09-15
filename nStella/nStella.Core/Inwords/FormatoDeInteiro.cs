0601dad1ad19d2a8b130991269122cc1f3925a16
heitor.barros <heitor@dilady.com.br>
2016-09-15 14:55:57 -0300

    Adicionado o formato em inteiro.

---
+++ nStella/nStella.Core/Inwords/FormatoDeInteiro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Inwords
+{
+    public class FormatoDeInteiro : IFormatoDeExtenso
+    {
+        public int GetCasasDecimais()
+        {
+            return 3;
+        }
+
+        public string GetUnidadeDecimalNoPlural()
+        {
+            return "milésimos";
+        }
+
+        public string GetUnidadeDecimalNoSingular()
+        {
+            return "milésimo";
+        }
+
+        public string GetUnidadeInteiraNoPlural()
+        {
+            return "inteiros";
+        }
+
+        public string GetUnidadeInteiraNoSingular()
+        {
+            return "inteiro";
+        }
+    }
+}
---
+++ nStella/nStella.Core/Inwords/IFormatoDeExtenso.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Inwords
+{
+    public interface IFormatoDeExtenso
+    {
+        /// <returns>A unidade no singular da parte inteira do número.</returns>
+        string GetUnidadeInteiraNoSingular();
+        /// <returns>A unidade no singular da parte inteira do número.</returns>
+        string GetUnidadeInteiraNoPlural();        
+        /// <returns>A unidade no singular da parte decimal do número.</returns>
+        string GetUnidadeDecimalNoSingular();
+        /// <returns>A unidade no plural da parte decimal do número.</returns>
+        string GetUnidadeDecimalNoPlural();        
+        /// <returns>A quantidade de casas decimais a serem consideradas 
+        /// na transfomação em extenso. </returns>
+        int GetCasasDecimais();
+    }
+}
