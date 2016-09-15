8d91a5f2f79c818d3d583bee68a316394d9a88ca
heitor.barros <heitor@dilady.com.br>
2016-09-15 14:28:21 -0300

    LengthError

---
+++ nStella/nStella.Core/Validation/Error/LengthError.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Validation.Error
+{
+    public class LengthError : IInvalidValue
+    {
+        private readonly int validLength;
+
+        public LengthError(int validLength)
+        {
+            this.validLength = validLength;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 31;
+            int result = 1;
+            result = prime * result + validLength;
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj)
+                return true;
+
+            if (obj == null)
+                return false;
+
+            if (!(obj is LengthError))
+                return false;
+
+            LengthError other = (LengthError)obj;
+
+            if (validLength != other.validLength)
+                return false;
+
+
+            return true;
+        }
+
+        public int ValidLength
+        {
+            get { return validLength; }
+        }
+
+        public string Name()
+        {
+            return "INVALID_LENGTH";
+        }
+    }
+}
---
+++ nStella/nStella.Core/Validation/LengthValidator.cs
@@ -0,0 +1,61 @@
+﻿using nStella.Core.Validation.Error;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Validation
+{
+    public class LengthValidator : IValidator<object>
+    {
+        private readonly int validLength;
+        private readonly BaseValidator baseValidator;
+
+        public LengthValidator(int validLength)
+        {
+            this.validLength = validLength;
+            baseValidator = new BaseValidator();
+        }
+
+        public LengthValidator(IMessageProducer messageProducer, int validLength)
+        {
+            baseValidator = new BaseValidator(messageProducer);
+            this.validLength = validLength;
+        }
+
+        private IList<IInvalidValue> GetInvalidValuesFor(object obj)
+        {
+            IList<IInvalidValue> messages = new List<IInvalidValue>();
+            if (obj.ToString().Length != validLength)
+            {
+                messages.Add(new LengthError(validLength));
+            }
+            return messages;
+        }
+
+        public void AssertValid(object Object)
+        {
+            baseValidator.AssertValid(GetInvalidValuesFor(Object));
+        }
+
+        public object GenerateRandomValid()
+        {
+            throw new NotSupportedException("Operação não suportada por este validador.");
+        }
+
+        public IList<IValidationMessage> InvalidMessageFor(object Object)
+        {
+            List<IValidationMessage> messages = new List<IValidationMessage>();
+            IList<IValidationMessage> msgs = baseValidator.GenerateValidationMessages(GetInvalidValuesFor(Object));
+            messages.AddRange(msgs);
+
+            return messages;
+        }
+
+        public bool IsEligible(object Object)
+        {
+            return true;
+        }
+    }
+}
