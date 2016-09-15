1e68404cd68081a2b905f46553ee99b7bb11efec
heitor.barros <heitor@dilady.com.br>
2016-09-15 14:00:35 -0300

    LeftSideZerosFormatter adicionado.

---
+++ nStella/nStella.Core/Format/LeftSideZerosFormatter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace nStella.Core.Format
+{
+    public class LeftSideZerosFormatter : IFormatter
+    {
+        private readonly int formattedLength;
+
+        public LeftSideZerosFormatter(int formattedLength)
+        {
+            this.formattedLength = formattedLength;
+        }
+
+        public bool CanBeFormatted(string value)
+        {
+            Regex regx = new Regex("\\d{" + formattedLength + "}");
+            return regx.IsMatch(value);
+        }
+
+        public string Format(string value)
+        {
+            if (!CanBeFormatted(value))
+                throw new ArgumentException("Argument value must have only " + formattedLength + " digits at most.");
+
+            string formated = value;
+            while (formated.Length < formattedLength)
+                formated = "0" + formated;
+
+            return formated;
+         }
+
+        public bool IsFormatted(string value)
+        {
+            Regex regx = new Regex("\\d{" + formattedLength + "}");
+            return regx.IsMatch(value);
+        }
+
+        public string UnFormat(string value)
+        {
+            int integer = Convert.ToInt32(value);
+            string formated = integer.ToString();
+            return formated;
+        }
+    }
+}
